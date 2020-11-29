df54fb82d3f9e500eea240ee857df7a364e2555d
Aviv Shvitzky <asifaviv@zahav.net.il>
2020-11-29 18:30:22 +0200

    a

---
+++ src/Router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter,
+  Redirect
+} from 'react-router-dom';
+
+import Countries from './pages/Countries'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/countries" />
+        </Route>
+        <Route exact default path="/countries"
+          component={Countries}
+        />
+      </Switch>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  )
+}
+
+export default Router
+
---
+++ src/pages/Countries.jsx
@@ -0,0 +1,20 @@
+// libraries and css
+import React from 'react';
+
+// components
+
+// consts
+
+// store
+
+
+function Countries() {
+
+  return (
+    <div className="countries">
+      test
+    </div>
+  );
+};
+
+export default Countries;
