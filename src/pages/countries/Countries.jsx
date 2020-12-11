8c67f3806c931df297a8d4539fca4225d9fc3be6
Aviv Shvitzky <asifaviv@zahav.net.il>
2020-12-11 17:19:40 +0200

    a

--- src/pages/countries/Countries.jsx
+++ src/pages/countries/Countries.jsx
@@ -10,46 +10,16 @@ import { fetchCountries } from '../../api/fetchFuncs'
 
 // store
 
-// const countriesByContinents = {
-//   Africa: [],
-//   Europe: [],
-//   Asia: [],
-//   Americas: [],
-//   Oceania: [],
-//   Polar: []
-// }
-
-const COUNTRIES_MODEL = {
-  Africa: [],
-  Americas: [],
-  Asia: [],
-  Europe: [],
-  Oceania: [],
-  Polar: [],
-  '': []
-}
-
 function Countries() {
   const [countries, setCountries] = useState({})
 
-  useEffect(async () => {
-    const getCountries = async () => {
+  useEffect(() => {
+     async function getCountries() {
       const fectchedCountries = await fetchCountries()
-      // setCountries(prevCountries => fectchedCountries.reduce((acc, country) => {
-      //   acc[country.region] = (acc[country.region] ?? []).concat(country);
-      //   return acc;
-      // }, {...prevCountries}))
-
-      const tmpCountries = {}
-      fectchedCountries.forEach(country => {
-          (tmpCountries[country.region] ?? []).concat(country)
-        // if (tmpCountries[country.region] === undefined) {
-        //   tmpCountries[country.region] = [country]
-        // } else {
-        //   tmpCountries[country.region].push(country)
-        // }
-      })
-      setCountries(tmpCountries)
+      setCountries(prevCountries => fectchedCountries.reduce((acc, country) => {
+        acc[country.region] = (acc[country.region] ?? []).concat(country);
+        return acc;
+      }, {...prevCountries}))
     }
     getCountries()
   }, [])
@@ -58,7 +28,7 @@ function Countries() {
     <div className="countries">
       countries page
       <div className="continent__box">
-        <div className="continent__name__box">Europe</div>
+        <div className="continent__name__box"><span>Europe</span></div>
         <div className="continent__countries__list">
           {countries.Europe &&
            countries.Europe.map(country => <span key={country.name}>{country.name}</span>)
