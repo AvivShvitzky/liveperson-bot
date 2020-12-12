10a3bf6dd218ce5badd928c2d6923bda77cbf30c
Aviv Shvitzky <asifaviv@zahav.net.il>
2020-12-12 20:11:11 +0200

    a

--- src/pages/country/Country.jsx
+++ src/pages/country/Country.jsx
@@ -1,7 +1,6 @@
 // libraries and css
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Country.css'
 
 // components
